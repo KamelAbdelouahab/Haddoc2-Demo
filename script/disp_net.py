c387d296646fd49170814f94ebc4a888455a1f24
Kamel Eddine ABDELOUAHAB <ke.abdelouahab@gmail.com>
2017-08-22 18:33:34 +0200

    Added scripts to generate HDL files using Haddoc2

---
+++ script/disp_net.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+import sys
+import os
+import io
+import numpy as np
+import math
+
+# Import caffe : defined in CAFFE_ROOT environment variable
+CAFFE_ROOT       = os.environ['CAFFE_ROOT']
+CAFFE_PYTHON_LIB = CAFFE_ROOT+'/python'
+sys.path.insert(0, CAFFE_PYTHON_LIB)
+os.environ['GLOG_minloglevel'] = '2' # Supresses Display on console
+import caffe;
+
+proto = 'caffe/deploy/test.prototxt';
+model = 'caffe/deploy/lenet.caffemodel';
+
+net = caffe.Net(proto,model,caffe.TEST);
+print 'Layer' + '\t|\t' + 'N' + '\t|\t' + 'Feature'
+print '-----' + '\t|\t' + '--' + '\t|\t' + '-------'
+
+
+for b in net.blobs.keys():
+    n = net.blobs[b].data.shape[1]
+    i = net.blobs[b].data.shape[2]
+    print b + '\t|\t' + str(n) + '\t|\t' + str(i) + 'x' + str(i)
+
+
---
+++ script/gen_hdl.sh
@@ -0,0 +1,5 @@
+$HADDOC2_ROOT/bin/haddoc2 \
+--proto=caffe/deploy/test.prototxt \
+--model=caffe/deploy/lenet.caffemodel \
+--out=hdl \
+--nbits=8
