b58e59c9b96dcf1871c6e0b13c640b35b4617751
Kamel ABDELOUAHAB <ke.abdelouahab@gmail.com>
2017-08-28 15:44:47 +0200

    Updated model names

--- script/gen_hdl.sh
+++ script/gen_hdl.sh
@@ -1,7 +1,7 @@
 mkdir -p haddocLib
 cp $HADDOC2_ROOT/lib/hdl/* haddocLib
 $HADDOC2_ROOT/bin/haddoc2 \
---proto=caffe/deploy/test.prototxt \
+--proto=caffe/deploy/lenet_conv.prototxt \
 --model=caffe/deploy/lenet.caffemodel \
 --out=hdl \
 --nbits=8
