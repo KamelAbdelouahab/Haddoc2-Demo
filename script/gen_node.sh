eaf8fd38ef2e26c1f3c06d3d8fd1dbcce30a7ce1
Kamel ABDELOUAHAB <ke.abdelouahab@gmail.com>
2017-09-06 16:49:54 +0200

    Features are correctly displayed, using gphwloop from GPStudio 7d92fa2e10d05664a553cf0d46e62ed8b37995e2

--- script/gen_node.sh
+++ script/gen_node.sh
@@ -1,21 +1,22 @@
 cd node
-rm -rf ./build/
-rm -rf *.node
-rm -rf Makefile
+#~ rm -rf ./build/
+#~ rm -rf *.node
+#~ rm -rf Makefile
 
 gpnode newproject -n demo
 gpnode setboard -n dreamcam_c3
 gpnode adddevice -n usb
+gpnode adddevice -n mt9
 
 gpnode addprocess -n lenet5 -d cnn.proc
 
-gpnode connect -f usb.out0 -t usb.in0
-gpnode connect -f usb.out1 -t lenet5.in
+gpnode connect -f usb.out0 -t lenet5.in
 gpnode connect -f lenet5.out -t usb.in1
+gpnode setproperty -n lenet5.enable -v 1
 
 gpnode generate -o ./build
 
 echo Succefully Generated Node ...
-make printfi
+# make printfi
 # make compile send
 # make compile send view
